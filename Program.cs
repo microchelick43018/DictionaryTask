a30f2b925aa3c219c1e895754d5a2fe40abcd2b6
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-11 12:40:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DictionaryTask
+{
+    class Program
+    {
+        static int MakeAChoice()
+        {
+            int choice = 0;
+            bool choiceIsNotCorrected = true;
+            while (choiceIsNotCorrected)
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice > 3 || choice < 0)
+                {
+                    Console.Write("Повторите ввод: ");
+                }
+                else
+                {
+                    choiceIsNotCorrected = false;
+                }
+            }
+            return choice;
+        }
+        static void ShowMenu()
+        {
+            Console.WriteLine("1 - Добавить в словарь новое слово");
+            Console.WriteLine("2 - Найти слово;");
+            Console.WriteLine("3 - Выход.");
+        }
+        static void AddWord(ref Dictionary<string, string> words)
+        {
+            string word;
+            string meaning;
+            Console.WriteLine("Введите слово");
+            word = Console.ReadLine();
+            Console.WriteLine("Введите значение этого слова");
+            meaning = Console.ReadLine();
+            words.Add(word, meaning);
+        }
+        static void FindWord(Dictionary<string, string> words)
+        {
+            Console.WriteLine("Введите слово, которое хотите найти: ");
+            string keyWord = Console.ReadLine();
+            if(words.ContainsKey(keyWord))
+            {
+                Console.WriteLine($"Значение: {words[keyWord]}");
+            }
+            else
+            {
+                Console.WriteLine("В словаре такого слова нет!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            bool choiceIsNotExit = true;
+            while(choiceIsNotExit)
+            {
+                ShowMenu();
+                switch (MakeAChoice())
+                {
+                    case 1:
+                        AddWord(ref words);
+                        break;
+                    case 2:
+                        FindWord(words);
+                        break;
+                    case 3:
+                        choiceIsNotExit = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
